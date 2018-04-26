9cc24e767981759581c6333c433e0ddbd459853a
kurokoji <shu.kakihana@gmail.com>
2018-04-26 21:33:57 +0900

    [add] イベントリスナ(画面遷移)用のjs追加

---
+++ trans.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const electron = require('electron');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const url = 'https://shinycolors.enza.fun/';
+
+    const webview = document.getElementById('webview');
+
+    const back = document.getElementById('back');
+    const forward = document.getElementById('forward');
+    const home = document.getElementById('home');
+
+    back.addEventListener('click', function() {
+        webview.goBack();
+    }, false);
+
+    forward.addEventListener('click', function() {
+        webview.goForward();
+    }, false);
+
+    home.addEventListener('click', function() {
+        webview.loadURL(url + 'home');
+    }, false);
+});
